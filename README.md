0d5a9f26376170aa5ee81bd1fd13253f8d825cc4
Sébastien Santoro <dereckson@espace-win.org>
2018-04-06 02:49:01 +0000

    Run test utilities suite
    Allow to run the utilities to get a sysinfo report
    and evaluate if the results are coherent.

---
+++ README.md
@@ -0,0 +1,2 @@
+Test utilities to ensure methods to fetch system information for rus sys-info crate
+will work co√rrectly on several FreeBSD systems.
--- get-boottime.c
+++ get-boottime.c
@@ -16,14 +16,8 @@ struct timeval get_boottime () {
     return read_timeval_sysctl_value("kern.boottime");
 }
 
-#define NSEC_PER_USEC   1000        /* nanoseconds per microsecond */
-#define USEC_PER_SEC    1000000     /* microseconds per second */
-#define NSEC_PER_SEC    1000000000  /* nanoseconds per second */
-#define timeval2nsec(tv) (tv.tv_sec * NSEC_PER_SEC + tv.tv_usec * NSEC_PER_USEC)
-
 int main (int argc, char *argv[]) {
     struct timeval boottime = get_boottime();
-    printf("%ld\n", timeval2nsec(boottime));
     printf("%ld.%06ld\n", boottime.tv_sec, boottime.tv_usec);
     return 0;
 }
